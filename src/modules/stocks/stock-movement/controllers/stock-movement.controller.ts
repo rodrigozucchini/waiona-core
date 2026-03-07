69cb7b7418d0923a3b27b7d219ed6f8df96d3a09
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-03-07 21:57:36 +0000

    add stock-movement

---
+++ src/modules/stocks/stock-movement/controllers/stock-movement.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+import { StockMovementService } from '../services/stock-movement.service';
+import { StockMovementResponseDto } from '../dto/stock-movement-respose.dto';
+
+@Controller('stock-movements')
+export class StockMovementController {
+
+  constructor(
+    private readonly stockMovementService: StockMovementService,
+  ) {}
+
+  @Get()
+  async findAll(): Promise<StockMovementResponseDto[]> {
+    return this.stockMovementService.findAll();
+  }
+
+  @Get(':id')
+  async findById(
+    @Param('id') id: number,
+  ): Promise<StockMovementResponseDto> {
+    return this.stockMovementService.findById(id);
+  }
+
+  @Get('stock-item/:stockItemId')
+  async findByStockItemId(
+    @Param('stockItemId') stockItemId: string,
+  ): Promise<StockMovementResponseDto[]> {
+    return this.stockMovementService.findByStockItemId(stockItemId);
+  }
+
+}
