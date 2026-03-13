b917ba8d408d3a1828380a8ba1a10933daa89214
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-03-13 03:17:27 +0000

    add module of pricing

---
+++ src/modules/pricing/entities/product-pricing.entity.ts
@@ -0,0 +1,36 @@
+import { BaseAuditEntity } from "src/common/entities/base.audit.entity";
+import { CurrencyCode } from "src/common/enums/currency-code.enum";
+import { ProductEntity } from "src/modules/products/product/entities/product.entity";
+import { Column, Entity, Index, JoinColumn, ManyToOne } from "typeorm";
+
+@Entity('product_pricing')
+@Index(['productId'], { unique: true })
+export class ProductPricingEntity extends BaseAuditEntity {
+
+  @Column({
+    name: 'product_id',
+    type: 'int',
+  })
+  productId: number;
+
+  @ManyToOne(() => ProductEntity, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'product_id' })
+  product: ProductEntity;
+
+  @Column({
+    type: 'enum',
+    enum: CurrencyCode,
+  })
+  currency: CurrencyCode;
+
+  @Column({
+    name: 'unit_price',
+    type: 'decimal',
+    precision: 12,
+    scale: 2,
+  })
+  unitPrice: number;
+
+}
