ce032b9e8902dd7af1f9466f9e93fb1844146542
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-04-15 01:43:31 +0000

    changing the dto of products

--- src/modules/products/categories/dto/update-category.dto.ts
+++ src/modules/products/categories/dto/update-category.dto.ts
@@ -1,30 +1,4 @@
-import {
-    IsString,
-    IsOptional,
-    IsBoolean,
-    IsInt,
-    Min,
-    MaxLength,
-  } from 'class-validator';
-  
-  export class UpdateCategoryDto {
-  
-    @IsOptional()
-    @IsString()
-    @MaxLength(100)
-    name?: string;
-  
-    @IsOptional()
-    @IsString()
-    @MaxLength(255)
-    description?: string;
-  
-    @IsOptional()
-    @IsBoolean()
-    isActive?: boolean;
-  
-    @IsOptional()
-    @IsInt()
-    @Min(1)
-    parentId?: number | null;
-  }
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCategoryDto } from './create-category.dto';
+
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
--- src/modules/products/product/dto/update-product.dto.ts
+++ src/modules/products/product/dto/update-product.dto.ts
@@ -1,42 +1,4 @@
-import {
-    IsString,
-    IsBoolean,
-    IsOptional,
-    IsEnum,
-    IsNumber,
-    Min,
-    MaxLength,
-  } from 'class-validator';
-  
-  import { ProductMeasurementUnit } from '../enums/product-measurement-unit.enum';
-  
-  export class UpdateProductDto {
-  
-    @IsOptional()
-    @IsString()
-    @MaxLength(50)
-    sku?: string;
-  
-    @IsOptional()
-    @IsString()
-    @MaxLength(150)
-    name?: string;
-  
-    @IsOptional()
-    @IsString()
-    @MaxLength(255)
-    description?: string;
-  
-    @IsOptional()
-    @IsBoolean()
-    isActive?: boolean;
-  
-    @IsOptional()
-    @IsEnum(ProductMeasurementUnit)
-    measurementUnit?: ProductMeasurementUnit;
-  
-    @IsOptional()
-    @IsNumber()
-    @Min(0)
-    measurementValue?: number;
-  }
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateProductDto } from './create-product.dto';
+
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
