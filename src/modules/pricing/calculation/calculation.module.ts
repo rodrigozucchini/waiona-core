facb0f406cd31151b2010f5418fb0f3d77ebd24d
rodrigozucchini <rodrigozucchini1997@gmail.com>
2026-04-22 02:00:50 +0000

    fixing calculation into the  pricing

--- src/modules/pricing/calculation/calculation.module.ts
+++ src/modules/pricing/calculation/calculation.module.ts
@@ -9,6 +9,8 @@ import { TaxEntity } from 'src/modules/taxation/taxes/entities/tax.entity';
 import { DiscountProductTargetEntity } from 'src/modules/discounts/discount-product-target/entities/discount-product-target.entity';
 import { DiscountComboTargetEntity } from 'src/modules/discounts/discount-combo-target/entities/discount-combo-target.entity';
 import { CouponEntity } from 'src/modules/coupons/coupon/entities/coupon.entity';
+import { CouponProductTargetEntity } from 'src/modules/coupons/coupon-product-target/entities/coupon-product-target.entity';
+import { CouponComboTargetEntity } from 'src/modules/coupons/coupon-combo-target/entities/coupon-combo-target.entity';
 
 import { CalculationService } from './services/calculation.service';
 import { CalculationController } from './controllers/calculation.controller';
@@ -24,6 +26,8 @@ import { CalculationController } from './controllers/calculation.controller';
       DiscountProductTargetEntity,
       DiscountComboTargetEntity,
       CouponEntity,
+      CouponProductTargetEntity,  // 🔥 para validar targets en applyCoupon
+      CouponComboTargetEntity,    // 🔥 para validar targets en applyCoupon
     ]),
   ],
   controllers: [CalculationController],
